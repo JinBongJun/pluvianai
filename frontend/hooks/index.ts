8aa028674c9ac0fe2d54e4351bc51549b60a962a
Jin Bong Jun <bongjun0289@daum.net>
2026-01-30 00:27:13 +0900

    feat: pivot to Sharp Wedge (Time Machine) strategy and core service cleanup preparation

--- frontend/hooks/index.ts
+++ frontend/hooks/index.ts
@@ -4,8 +4,8 @@
 export { useAsyncData, useAsyncFetch } from './useAsyncData';
 export type { AsyncState, UseAsyncDataOptions, UseAsyncDataReturn } from './useAsyncData';
 
-export { useDebounce } from './useDebounce';
+export { useDebouncedValue } from './useDebounce';
 export { useFormSubmit } from './useFormSubmit';
-export { useKeyboardShortcuts } from './useKeyboardShortcuts';
+export { useKeyboardShortcuts, useGlobalShortcuts } from './useKeyboardShortcuts';
 export { useLoading } from './useLoading';
 export { useScrollAnimation } from './useScrollAnimation';
